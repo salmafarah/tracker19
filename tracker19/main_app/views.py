aa71af1d1dadfe11ad432c95bede82aa938bf892
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-04-13 12:29:13 -0400

    added template functions

--- tracker19/main_app/views.py
+++ tracker19/main_app/views.py
@@ -1,11 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
-def home(request):
-    return HttpResponse('<h1>Welcome to the Tracker19 Home Page</h1>')
+#Classes go here
+
+
+
+# Create your views here.
+def home(request): #shows all generic info 
+  pass
+    # return render(request, 'home.html')
+
+
+
+
+
+
 
 def about(request):
   return render(request, 'about.html')
+
+
+
+
+
+
+
+
+def signin(request): #sign-in page
+  pass
+
+
+
+
+
+
+
+
+def profile(request): #shows all entries of the user
+  pass
+
+
+
+
+
+
+
+
+def create_form(request): #info goes here  
+  pass
+
+
+
+
+
+
+
+
+def one_entry(request): #shows one entry when clicked on #CREATE DELETE AND UPDATE goes here
+  pass
+
+
+
+
+
+
+
 
