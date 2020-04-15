ec11922d0d9b11fa2c3b07845393be3aaedbed57
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-04-15 11:58:53 -0400

    test ground

--- tracker19/main_app/models.py
+++ tracker19/main_app/models.py
@@ -8,13 +8,10 @@
 from django.db import models
 from django.contrib.auth.models import User 
 from django.urls import reverse
+# import datetime
+
 
 class Entry (models.Model) : 
-    year = models.IntegerField()
-    month = models.IntegerField()
-    day = models.IntegerField()
-    hour = models.IntegerField()
-    min = models.IntegerField()
     location = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     partner = models.CharField(max_length=100)
@@ -24,3 +21,8 @@ class Entry (models.Model) :
     def __str__(self):
         return self.location
 
+#     year = models.IntegerField()
+#     month = models.IntegerField()
+#     day = models.IntegerField()
+#     hour = models.IntegerField()
+#     min = models.IntegerField()
