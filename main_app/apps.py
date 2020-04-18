f2d31971a49dc95aff55d43350c8423f42ec87b8
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-04-18 11:28:00 -0400

    fixing merge conflicts

---
+++ main_app/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class Track19Config(AppConfig):
+    name = 'track19'
---
+++ main_app/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('accounts/signup/', views.signup, name='signup'),
+  path('health/', views.health_index, name='health_index'),
+  path('health/<int:health_id>/', views.health_detail, name='health_detail'),
+  path('health/create/', views.HealthCreate.as_view(), name='health_create'),
+  path('health/<int:pk>/update/', views.HealthUpdate.as_view(), name='health_update'),
+  path('health/anonymous/', views.anonymous, name='anonymous'),
+  path('entry/', views.entry_index, name='entry_index'),
+  path('entry/<int:entry_id>/', views.entry_detail, name='detail'),
+  path('entry/create/', views.FormCreate.as_view(), name='form_create'),
+  path('entry/<int:entry_id>/assoc_partner/<int:partner_id>/', views.assoc_partner, name='assoc_partner'),
+  path('entry/<int:entry_id>/unassoc_partner/<int:partner_id>/', views.unassoc_partner, name='unassoc_partner'),
+  ################################PARTNER VIEWS###########################################
+  path('partner/', views.PartnerList.as_view(), name='partner_index'),
+  path('partner/<int:pk>/', views.PartnerDetail.as_view(), name='partner_detail'),
+  path('partner/create/', views.PartnerCreate.as_view(), name='partner_create'),
+  path('partner/<int:pk>/update/', views.PartnerUpdate.as_view(), name='partner_update'),
+  path('partner/<int:pk>/delete/', views.PartnerDelete.as_view(), name='partner_delete'),
+  ################################LOCATION VIEWS###########################################
+  path('entry/<int:pk>/update/', views.EntryUpdate.as_view(), name='entry_update'),
+  path('entry/<int:pk>/delete/', views.EntryDelete.as_view(), name='entry_delete'),
+  path('entry/<int:entry_id>/picked_location/<int:location_id>/', views.picked_location, name='picked_location'),
+  path('entry/<int:entry_id>/location_unpicked/<int:location_id>/', views.unpicked_location, name='unpicked_location'),
+  path('location/', views.LocationList.as_view(), name='location_index'),
+  path('location/<int:pk>/', views.LocationDetail.as_view(), name='location_detail'),
+  path('location/create/', views.LocationCreate.as_view(), name='location_create'),
+  path('location/<int:pk>/update/', views.LocationUpdate.as_view(), name='location_update'),
+  path('location/<int:pk>/delete/', views.LocationDelete.as_view(), name='location_delete'),
+]
